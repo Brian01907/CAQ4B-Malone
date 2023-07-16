a3615d9a483c86bdcad67d45ee09bfd5f9288236
Brian01907 <brianmalone26@gmail.com>
2023-07-16 13:03:33 +0000

    CAQ4
    Signed-off-by: Brian01907 <brianmalone26@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, c = 30;
+    int d, e;
+
+    //  a + b
+    d = a + b;
+    printf("Value of d: %d\n", d);
+
+    //  c - d
+    e = d - c;
+    printf("Value of e: %d\n", e);
+
+    return 0;
+}
